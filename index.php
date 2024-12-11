c87c3fcb44925e0a5345ce09f701016a5f1c0b94
lgmb <luizbiel222@gmail.com>
2024-12-11 09:33:46 -0300

    atualização index

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
 chdir(__DIR__);
-require_once( __DIR__ . "/Routes/routes.php");
-// Inclui o roteador
+require_once(__DIR__ . "/Routes/routes.php");
 
-$route = $_SERVER['REQUEST_URI'];// Obtém a rota requisitada
-$method = $_SERVER['REQUEST_METHOD']; // Obtém o método HTTP usado
+$route = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
 
-// Roteador processa a requisição
+// Remover query strings, caso existam
+if (strpos($route, '?') !== false) {
+    $route = explode('?', $route)[0];
+}
+
+// Processa a requisição
 handleRequest($route, $method);
